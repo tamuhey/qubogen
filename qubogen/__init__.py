504683ce7efd148278a3c9b0439eef4cef0059fe
Yohei Tamura <tamuhey@gmail.com>
2019-02-26 22:51:28 +0900

    cln and start to use setup.cfg

--- qubogen/__init__.py
+++ qubogen/__init__.py
@@ -2,3 +2,5 @@
 from .data import *
 from .qubo import *
 from .utils import *
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='qubogen',
-    version='0.1.0',
-    packages=find_packages(),
-    url='https://github.com/tamuhey/qubogen',
-    install_requires=["networkx"],
-    license='MIT',
-    author='tamuhey',
-    author_email='tamuhey@gmail.com',
-    description='QUBO matrix generator on major combinatorial optimization problems written in Python',
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"]
-)
+setup()
